a20f64e3439e15ac6a194506b66fb0fece6f1e1b
Timur Boltachev <timur.boltachev@joy-dev.com>
2021-04-19 19:42:43 +0300

    second phase

--- app/src/main/java/com/dester/a2chbest/fragment/sub_category/SubCategoryFragment.kt
+++ app/src/main/java/com/dester/a2chbest/fragment/sub_category/SubCategoryFragment.kt
@@ -2,6 +2,4 @@ package com.dester.a2chbest.fragment.sub_category
 
 import com.dester.a2chbest.base.BaseFragment
 
-class SubCategoryFragment : BaseFragment<SubCategoryVM>() {
-
-}
+class SubCategoryFragment : BaseFragment<SubCategoryVM>()
